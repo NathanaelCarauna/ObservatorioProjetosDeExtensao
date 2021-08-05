4bfa17e8abe5580d8bfec613ec222d54e36fdd6b
NathanaelCarauna <Nathanael.carauna@gmail.com>
2021-08-04 23:39:12 -0300

    UsuarioController criado

--- src/main/java/application/controller/UsuarioController.java
+++ src/main/java/application/controller/UsuarioController.java
@@ -1,5 +1,55 @@
 package application.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import application.model.Usuario;
+import application.service.UsuarioService;
+
+@Controller
+@RequestMapping("/api/usuario")
 public class UsuarioController {
 
+	@Autowired
+	private UsuarioService service;
+	
+	@GetMapping
+	public ResponseEntity<List<Usuario>> getAll(){
+		return ResponseEntity.ok(service.getAll());
+	}
+	
+	@GetMapping("/{id}")
+	public ResponseEntity<Usuario> getById(@PathVariable long id){
+		var usuario = service.getById(id);
+		return ResponseEntity.ok(usuario);
+	}
+	
+	@PostMapping()
+	public ResponseEntity<Usuario> createUsuario(@RequestBody Usuario usuarioParam){
+		var usuario = service.createUsuario(usuarioParam);
+		return ResponseEntity.ok(usuario);
+	}
+	
+	@PutMapping("/{id}")
+	public ResponseEntity<Usuario> updateUsuario(@PathVariable long id, @RequestBody Usuario usuario){
+		var usuario = service.updateUsuario(id, usuario);
+		return ResponseEntity.ok(usuario);
+	}
+	
+	@DeleteMapping("/{id}")
+	public ResponseEntity<Usuario> deleteUsuario(@PathVariable long id){
+		var usuario = service.deleteUsuario(id);
+		return ResponseEntity.ok(usuario);
+	}
+	
 }
