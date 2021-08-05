01b6d9c951c849c6b647c390ef678e7d10432b5b
NathanaelCarauna <Nathanael.carauna@gmail.com>
2021-08-04 23:14:34 -0300

    Model usuario criado

--- src/main/java/application/model/Usuario.java
+++ src/main/java/application/model/Usuario.java
@@ -1,5 +1,19 @@
 package application.model;
 
-public class Usuario {
+import java.util.List;
 
+import javax.persistence.Entity;
+
+import lombok.Data;
+
+@Entity
+@Data
+public class Usuario {	
+	private long id;
+	private String nome;
+	private String sobrenome;
+	private String email;
+	private String password;
+	private List<Projeto> projetos;
+	
 }
