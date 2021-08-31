468f27db61321535cb28e487cf861370e12ea11a
DahiseDias <dahisedias@gmail.com>
2021-08-31 10:15:28 -0300

    tela ususario finalizada

--- src/main/front/src/Components/cadastro.jsx
+++ src/main/front/src/Components/cadastro.jsx
@@ -1,29 +1,17 @@
 export const Cadastro = (props) => {
     return (
         <div id='about'>
-            <div className="container">
+            <div className="container" id='formulario'>
                 <div className="conteudo">
                     <form className="formulario">
-                        <legend className="texto">Cadastro</legend>
-                        <label className="label" for="email">Email</label>
+                        <label className="texto">Cadastro</label>
+                        <label id='legenda' for="email">Email</label>
                         <input type="email" name="email" id="Email" placeholder="escreva seu email" />
-                        <label className="label" for="password">Senha</label>
+                        <label id='legenda' for="password">Senha</label>
                         <input type="password" name="password" id="Password" placeholder="escreva sua senha" />
 
-                        <legend className="texto">Tipo de Usuario</legend>
-                        <label className="texto" check>
-                            <input type="radio" name="tipo" />{' '}
-                            Psicologo
-                        </label>
-
-                        <label className="texto" check>
-                            <input type="radio" name="tipo" />{' '}
-                            Paciente
-                        </label>
                     </form>
-                </div>
-                <div className="botoes">
-                    <button className="botao" size="lg" > Cadastrar </button>{' '}
+                    <button className="botao" > Cadastrar </button>{' '}
                 </div>
             </div>
 
