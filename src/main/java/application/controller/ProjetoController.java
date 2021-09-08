9f032440267572093bbe221c151e2d8915a45c66
DahiseDias <dahisedias@gmail.com>
2021-09-08 19:47:57 -0300

    tentando consertar a rota

--- src/main/java/application/controller/ProjetoController.java
+++ src/main/java/application/controller/ProjetoController.java
@@ -24,6 +24,18 @@ public class ProjetoController {
 	
 	@Autowired
 	private ProjetoService service;
+	@GetMapping("/dados")
+	public ResponseEntity<dadosDTO> getDados(){
+		List<Projeto> projeto;
+		try {
+			projeto = service.getAll();
+			dadosDTO dados = new dadosDTO(projeto);
+			return ResponseEntity.ok(dados);
+		} catch (Exception e) {
+			e.printStackTrace();
+			return ResponseEntity.notFound().build();
+		}
+	}
 	
 	@GetMapping
 	public ResponseEntity<List<Projeto>> getAll(){
@@ -71,17 +83,6 @@ public class ProjetoController {
 			return ResponseEntity.notFound().build();
 		}
 	}
-	@GetMapping("/dados")
-	public ResponseEntity<dadosDTO> getDados(){
-		List<Projeto> projeto;
-		try {
-			projeto = service.getAll();
-			dadosDTO dados = new dadosDTO(projeto);
-			return ResponseEntity.ok(dados);
-		} catch (Exception e) {
-			e.printStackTrace();
-			return ResponseEntity.notFound().build();
-		}
-	}
+	
 	
 }
